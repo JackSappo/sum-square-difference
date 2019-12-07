fee4017bd701ef4fe02111da7a482c22fe05f3fb
Jack <jisapp02@gmail.com>
2019-12-07 15:46:00 -0500

    Input and button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Edit <code>src/App.js</code> and save to reload.
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: '',
+      networkRequests: []
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSend = this.onSend.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({ inputValue: e.target.value });
+  }
+
+  onSend() {
+    console.log('Sending', this.state.inputValue);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input value = {this.state.inputValue || ''} type="number" onChange={this.handleInputChange} />
+        <button onClick={this.onSend}>Send</button>
+      </div>
+    );
+  }
 }
 
 export default App;
