e4fcff57ee42995e458c11dc181fb856d28aac59
Jack <jisapp02@gmail.com>
2019-12-08 07:51:29 -0500

    loading animation

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
 
 export function UserInput(props) {
   const { inputValue = '', loading, onInputChange, calculate, solutionValue } = props;
@@ -10,8 +11,20 @@ export function UserInput(props) {
         <button onClick={calculate}>Calculate</button>
       </div>
       <div className="solution">
-        <b>Solution</b>: {loading ? 'LOADING': solutionValue}
+        <b>Solution</b>: {loading ? renderLoader() : solutionValue}
       </div>
     </div>
   );
+}
+
+function renderLoader() {
+  return (
+    <Loader
+      visible={true}
+      type="TailSpin"
+      color="#000000"
+      height={14}
+      width={14}
+    />
+   );
 }
