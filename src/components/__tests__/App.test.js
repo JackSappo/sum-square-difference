96dfc27cf42cdb78917336949ca6962ff53e3597
Jack <jisapp02@gmail.com>
2019-12-07 21:41:42 -0500

    enzyme setup

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('tests', () => {
+  it('does a test', () => {
+    const wrapper = shallow(<App />)
+
+    expect(5).toEqual(5);
+  })
+})
